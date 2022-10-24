4ab58126c97f861b9ccaa8dc31aca360ff4f0cd5
MoyoSharon <85716560+MoyoSharon@users.noreply.github.com>
2022-10-24 11:13:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+function openNav() {
+  document.getElementById("nav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+}
